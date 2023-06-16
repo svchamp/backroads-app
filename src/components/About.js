c8bf97ea19ef74dfdb05d35ab5dd8ed87a36ce97
Shrinivas <svchampf510@gmail.com>
2023-06-17 04:21:14 +0530

    about

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ const about = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#" className="btn">
+          <a href="#home" className="btn">
             read more
           </a>
         </article>
